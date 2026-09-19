bbd9a9a41efe271f08358d981f94438e92e091d8
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2026-09-19 17:27:01 +0200

    Bump indications custom element to r49

--- custom_components/investment/www/investment-panel.js
+++ custom_components/investment/www/investment-panel.js
@@ -2950,4 +2950,4 @@ class InvestmentPanel extends HTMLElement {
   `;}
 }
 
-if(!customElements.get("investment-panel-r48")) customElements.define("investment-panel-r48",InvestmentPanel);
+if(!customElements.get("investment-panel-r49")) customElements.define("investment-panel-r49",InvestmentPanel);
